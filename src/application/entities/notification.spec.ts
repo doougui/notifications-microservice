d97cfa67706a711679f1b0ab4ad8307b24f47d88
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-12-14 10:07:09 -0300

    creating notification entity

---
+++ src/application/entities/notification.spec.ts
@@ -0,0 +1,15 @@
+import { Content } from './content';
+import { Notification } from './notification';
+
+describe('Notification', () => {
+  it('should be able to create a notification', () => {
+    const content = new Notification({
+      content: new Content('Você recebeu uma solicitação de amizade.'),
+      category: 'social',
+      recipientId: 'example-recipient-id',
+      createdAt: new Date(),
+    });
+
+    expect(content).toBeTruthy();
+  });
+});
