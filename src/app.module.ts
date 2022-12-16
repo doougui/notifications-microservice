4dcb4b8890e43bc19e1d3dc4bd7fd33cd38d8530
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-12-16 15:07:55 -0300

    adding kafka

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
+import { MessagingModule } from '@infra/messaging/messaging.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './infra/database/database.module';
 import { HttpModule } from './infra/http/http.module';
 
 @Module({
-  imports: [HttpModule, DatabaseModule],
+  imports: [
+    HttpModule,
+    DatabaseModule,
+    MessagingModule,
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
